6a82d8a4fc7e8803387ecce3cbd9e3754f89f8ec
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2023-11-19 21:26:54 +0100

    portafolio y Agencia.js

--- travel-life/maquetacionAgencia.js
+++ travel-life/maquetacionAgencia.js
@@ -11,4 +11,23 @@ function pickInfo(){
                 'email': document.getElementById('email').value
         }
         info_array.push(info);
+        console.log(info_array);
+}
+
+
+function filter(){
+        let filteredInfo = '';
+        info_array.forEach(function(info) {
+                if((info.destination.toUpperCase() == 'MALLORCA')
+                || (info.destination.toUpperCase() == 'GALICIA') 
+                || (info.destination.toUpperCase() == 'CANARIAS') ) {
+                        filteredInfo += '<p>Nombre: ' + info.name + '</p>' +
+                                        '<p>Origen: ' + info.origin + '</p>' +
+                                        '<p>Destino: ' + info.destination + '</p>' +
+                                        '<p>Número de personas: ' + info.paxes + '</p>' +
+                                        '<p>Fecha: ' + info.date + '</p>' +
+                                        '<p>Correo electrónico: ' + info.email + '</p>';
+                }
+        });
+        document.getElementById("showInfo").innerHTML = filteredInfo;
 }
