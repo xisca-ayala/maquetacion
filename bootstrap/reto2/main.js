02b77450f5da5806b08ca73340c31043c4488ced
xisca-ayala <xisca.ayala@iMac-de-Antoni.local>
2023-11-28 15:44:36 +0100

    funcionalidad

--- bootstrap/reto2/main.js
+++ bootstrap/reto2/main.js
@@ -17,44 +17,26 @@ $(document).ready(function(){
         $("#modalBody").prepend('<p id="clothesData1" class="modal-text"><span id="clothesModal1" class="modal-text"> Pantalon suelto - </span><span id="price1">20</span></p>');
     });
     $("#clothes2").on("click", function(){
-        $("#modalBody").prepend('<span id="clothesModal2" class="modal-text"> Vestido Tunico - </span><span id="price1">18</span>');
+        $("#modalBody").prepend('<span id="clothesModal2" class="modal-text"> Vestido Tunico - </span><span id="price2">18</span>');
     });
     $("#clothes3").on("click", function(){
-        $("#modalBody").prepend('<span id="clothesModal3" class="modal-text"> Falda Mid - </span><span id="price1">18</span>');
+        $("#modalBody").prepend('<span id="clothesModal3" class="modal-text"> Falda Mid - </span><span id="price3">18</span>');
     });
     $("#clothes4").on("click", function(){
-        $("#modalBody").prepend('<span id="clothesModal4" class="modal-text"> Cardigan Largo - </span><span id="price1">18</span>');
+        $("#modalBody").prepend('<span id="clothesModal4" class="modal-text"> Cardigan Largo - </span><span id="price4">18</span>');
     });
     $("#clothes5").on("click", function(){
-        $("#modalBody").prepend('<span id="clothesModal5" class="modal-text"> Falda Animal - </span><span id="price1">18</span>');
+        $("#modalBody").prepend('<span id="clothesModal5" class="modal-text"> Falda Animal - </span><span id="price5">18</span>');
     });
     $("#clothes6").on("click", function(){
-        $("#modalBody").prepend('<span id="clothesModal6" class="modal-text"> Mono Beig - </span><span id="price1">18</span>');
+        $("#modalBody").prepend('<span id="clothesModal6" class="modal-text"> Mono Beig - </span><span id="price6">18</span>');
     });
     $("#clothes7").on("click", function(){
-        $("#modalBody").prepend('<span id="clothesModal7" class="modal-text"> Vestido gris - </span><span id="price1">18</span>');
+        $("#modalBody").prepend('<span id="clothesModal7" class="modal-text"> Vestido gris - </span><span id="price7">18</span>');
     });
     $("#clothes8").on("click", function(){
-        $("#modalBody").prepend('<span id="clothesModal8" class="modal-text"> Pantalón ancho - </span><span id="price1">18</span>');
+        $("#modalBody").prepend('<span id="clothesModal8" class="modal-text"> Pantalón ancho - </span><span id="price8">18</span>');
     });
     
 });
 
-{/* <h3 id="clothes1" class="mt-2"></h3>
-<h3 id="clothes1" class="mt-2"></h3>
-<h3 id="clothes1" class="mt-2"></h3>
-<h3 id="clothes1" class="mt-2"></h3>
-<h3 id="clothes1" class="mt-2"></h3>
-<h3 id="clothes1" class="mt-2"></h3>
-<h3 id="clothes1" class="mt-2"></h3>
-<h3 id="clothes1" class="mt-2"></h3> */}
-
-
-// let garment1 = new Clothes("Pantalón suelto",20);
-// let garment2 = new Clothes("Vestido Túnico",18);
-// let garment3 = new Clothes("Falda mid",12);
-// let garment4 = new Clothes("Cárdigan",22);
-// let garment5 = new Clothes("Falda animal",21);
-// let garment6 = new Clothes("Mono Beig",25);
-// let garment7 = new Clothes("Vestido gris",24);
-// let garment8 = new Clothes("Pantalón ancho",20);
